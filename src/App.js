cc6a36554203fa2aaa06260aa1f8ea4202482f7e
AkramMahmoudi <akram.mahmoudi135@gmail.com>
2024-11-19 17:47:16 +0300

    add footerv2 comp

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import Testimonial from "./components/Testimonial";
 import Faq from "./components/Faq";
 import ContactUs from "./components/ContactUs";
 // import DownloadSec from "./components/DownloadSec";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import Banner2 from "./components/Banner2";
+import FooterV2 from "./components/FooterV2";
 function App() {
   return (
     <>
@@ -25,163 +26,7 @@ function App() {
       <ContactUs />
       {/* <DownloadSec /> */}
       {/* <Footer /> */}
-      <>
-        {/*  footer */}
-        <footer>
-          <div className="footer">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-3 col-sm-6">
-                  <div className="infoma text_align_left">
-                    <h3 style={{ color: "white" }}>About</h3>
-                    <p style={{ color: "white" }} className="ipsum">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sedconsectetur{" "}
-                    </p>
-                    <ul className="social_icon">
-                      <li>
-                        <a
-                          href="/"
-                          onClick={(e) => {
-                            e.preventDefault(); // Prevent navigation
-                          }}
-                        >
-                          <i className="fa fa-facebook" aria-hidden="true" />
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="/"
-                          onClick={(e) => {
-                            e.preventDefault(); // Prevent navigation
-                          }}
-                        >
-                          <i className="fa fa-twitter" aria-hidden="true" />
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="/"
-                          onClick={(e) => {
-                            e.preventDefault(); // Prevent navigation
-                          }}
-                        >
-                          <i
-                            className="fa fa-linkedin-square"
-                            aria-hidden="true"
-                          />
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="/"
-                          onClick={(e) => {
-                            e.preventDefault(); // Prevent navigation
-                          }}
-                        >
-                          <i className="fa fa-instagram" aria-hidden="true" />
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="col-md-3 col-sm-6">
-                  <div className="infoma">
-                    <h3 style={{ color: "white" }}>Adderess</h3>
-                    <ul style={{ color: "white" }} className="conta">
-                      <li>
-                        Healing Center, oo W Street name, <br />
-                        Loram ipusum
-                        <br /> (+213) 8522369417 <br />
-                        (+213) 8522369417 <br />
-                      </li>
-                      {/* <li>
-                        (+213) 8522369417 <br />
-                        (+213) 8522369417
-                      </li> */}
-                      <li>
-                        <a className="emailss" href="/">
-                          demo@gmail.com
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="col-md-3 pad_lrft col-sm-6">
-                  <div className="infoma">
-                    <h3
-                      style={{
-                        color: "white",
-                        textAlign: "center",
-                        marginRight: "15%",
-                      }}
-                    >
-                      Links
-                    </h3>
-                    <ul className="fullink">
-                      <li>
-                        <a href="index.html">Home</a>
-                      </li>
-                      <li>
-                        <a href="/">About</a>
-                      </li>
-                      <li>
-                        <a href="/">Shop</a>
-                      </li>
-                      <li>
-                        <a href="/">Quality</a>
-                      </li>
-                      <li>
-                        <a href="/">Contact</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="col-md-3 col-sm-6">
-                  <div className="infoma">
-                    <h3 style={{ color: "white" }}>Newsletter</h3>
-                    <form className="form_subscri">
-                      <div className="row">
-                        <div className="col-md-12">
-                          <input
-                            className="newsl"
-                            placeholder="Your Name"
-                            type="text"
-                            name="Your Name"
-                          />
-                        </div>
-                        <div className="col-md-12">
-                          <input
-                            className="newsl"
-                            placeholder="Email"
-                            type="text"
-                            name="Email"
-                          />
-                        </div>
-                        <div className="col-md-12">
-                          <button className="subsci_btn">subscribe</button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="copyright">
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12">
-                    <p>
-                      © 2024 All Rights Reserved. <a href=""> Express Tech</a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
-        {/* end footer */}
-      </>
+      <FooterV2></FooterV2>
     </>
   );
 }
